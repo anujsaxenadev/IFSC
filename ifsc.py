eff3bdd6bad4ed97035c039c39d6f2e7e47a4bbe
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-26 20:18:30 +0530

    Validating and Printing response

--- ifsc.py
+++ ifsc.py
@@ -5,3 +5,14 @@ from prettytable import PrettyTable
 ifsc = input("Enter IFSC Code : ")
 url = "https://ifsc.razorpay.com/" + ifsc
 response = requests.get(url)
+
+# Validating and Priting the Response
+if response:
+    json = response.json()
+    table = PrettyTable(['Information', 'Details'])
+    for info, d in json.items():
+    	if d != "":
+    		table.add_row([info, d])
+    print(table)
+else:
+    print('An error has occurred. Please check the IFSC Code Again')
