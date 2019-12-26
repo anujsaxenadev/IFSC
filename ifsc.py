5ad3d6ad4c729cc3aca8b225aecda66a442d1cf9
Anuj Saxena <anuj.saxena.dev@gmail.com>
2019-12-26 20:17:37 +0530

    getting information from razorpay API

--- ifsc.py
+++ ifsc.py
@@ -1,2 +1,7 @@
 import requests
 from prettytable import PrettyTable
+
+# Getting the Response from Razorpay.com's API
+ifsc = input("Enter IFSC Code : ")
+url = "https://ifsc.razorpay.com/" + ifsc
+response = requests.get(url)
